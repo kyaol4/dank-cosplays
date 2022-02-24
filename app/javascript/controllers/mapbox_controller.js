320ecb63330711a2cbd8067cdba30de7008d6413
dongwook kim <kyaol4@gmail.com>
2022-02-24 15:39:17 +0900

    changed map color

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -12,7 +12,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/kyaol4/cl00m3kaz004114qrphwd7dv2"
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
